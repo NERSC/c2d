e072643e0cabf9bcd2e257e79a6f27cc82fb1e05
Kelly L Rowland <kellylynnerowland@gmail.com>
2019-04-25 11:44:11 -0700

    add setup script for pip install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='conda2docker',
+      version='0.1',
+      description='conda2docker utility',
+      author='Kelly L. Rowland',
+      author_email='kellyrowland@lbl.gov',
+      url='https://www.github.com/kellyrowland/c2d/',
+      scripts=['c2d'],
+     )
